471c02ea17c7b3746e8feff4f48f235dd86a7274
Gregorio <greg.massara@gmail.com>
2018-08-24 23:35:57 +0100

    Default instance added also to Fragment 2 in case of user not logged in

--- app/src/main/java/com/example/gregorio/greenjiin/Moped2Fragment.java
+++ app/src/main/java/com/example/gregorio/greenjiin/Moped2Fragment.java
@@ -1,14 +1,11 @@
 package com.example.gregorio.greenjiin;
 
-import static com.example.gregorio.greenjiin.Constants.REALM_BASE_URL;
-
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -102,12 +99,18 @@ public class Moped2Fragment extends Fragment {
   }
 
   private RealmResults<MopedModel> setUpRealm() {
-    SyncConfiguration configuration = SyncUser.current().getDefaultConfiguration();
-    Log.i(LOG_TAG,"SyncConfiguration 1: " + configuration.toString());
+
+    if (SyncUser.current() != null) {
+      SyncConfiguration configuration = SyncUser.current().getDefaultConfiguration();
+      Log.i(LOG_TAG, "SyncConfiguration 1: " + configuration.toString());
 
 //          .createConfiguration(REALM_BASE_URL + "/default")
 //          .build();
-    realm = Realm.getInstance(configuration);
+      realm = Realm.getInstance(configuration);
+    } else {
+      realm = Realm.getDefaultInstance();
+    }
+
 
     return realm
         .where(MopedModel.class)
